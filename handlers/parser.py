67ee5aab6e42f86e3bfa496124a146c7785c4c52
Admin <seremenko135@gmail.com>
2024-09-28 18:52:52 +0300

    Callback dont action

--- handlers/parser.py
+++ handlers/parser.py
@@ -1,8 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
-def get_crypto_price(coins):
-    result = {}
-    html_resp = requests.get('https://cryptorank.io/ru').text
-    block = BeautifulSoup(html_resp, 'lxml')
-    rows = block.find_all('tr', class_='sc-a321a998-0 QrTwc init-scroll')
+html_resp = requests.get('https://cryptorank.io').text
+block = BeautifulSoup(html_resp, 'lxml')
+rows = block.find_all('tr', class_='sc-a321a998-0 QrTwc init-scroll')
+
+price_btc = rows[0].findAll('p', class_='sc-b7cd6de0-0 sc-28f598be-1 tEDJi nZrBG')[0].text.strip()
+price_eth = rows[1].findAll('p', class_='sc-b7cd6de0-0 sc-28f598be-1 tEDJi nZrBG')[0].text.strip()
+
+ltc_resp = requests.get('https://cryptorank.io/price/litecoin').text
+check = BeautifulSoup(ltc_resp, 'lxml')
+price_ltc = check.findAll('div', class_='sc-cb748c3-0 cwWsJH')[0].text.strip()
